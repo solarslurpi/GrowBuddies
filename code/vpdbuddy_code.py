f88fd3209a6d0466199ecf8bb38152fde7e3f260
solarslurpi <happyday.mjohnson@gmail.com>
2022-11-01 16:05:18 -0700

    evolved.

--- code/vpdbuddy_code.py
+++ code/vpdbuddy_code.py
@@ -1,4 +1,4 @@
-import math
+
 import logging
 from enum import Enum
 from growbuddy_code import GrowBuddy
@@ -8,7 +8,7 @@ settings_filename = "code/growbuddy_settings.json"
 
 
 class growthStage(Enum):
-    """An enumeration to set the stage of plant growth.  Whether in vegetative of flower is an input from the user.
+    """An enumeration to set the stage of plant growth.  Whether in vegetative or flower is an input from the user.
     The code then converts this to a growthStage enum and passes the value when instantiating an instance of the
     VPDcontroller() class.
 
@@ -90,45 +90,6 @@ class vpdBuddy(GrowBuddy):
             dict["vpd"] = vpd
             dict["seconds_on"] = nSecondsON
             self.snifferbuddy_values_callback(dict)
-
-    def _calc_vpd(self, dict: dict) -> (float):
-
-        """INTERNAL METHOD. I decided at this point not to measure the leaf temperature but take the much simpler
-        approach of assuming 2 degrees F less than the air temperature.  Clearly not as accurate as reading.  But for
-        my purposes "good enough."
-
-        Once an mqtt message is received from the GrowBuddy broker that a SnifferBuddy reading is available, _calc_vpd()
-        is called to calculate the VPD.  The mqtt message comes in as a JSON string.  The JSON string is converted to a
-        dictionary.  The dictionary contains the values needed for the VPD calculation.
-
-        The VPD equation comes
-        `from a Quest website <https://www.questclimate.com/vapor-pressure-deficit-indoor-growing-part-3-different-stages-vpd/>`_
-
-        Args:
-            dict (dict): Dictionary of values returned from an mqtt message.
-        Raises:
-            Exception: If one of the values needed to calculate the VPD is of a type or value that won't work.
-
-        Returns the calculated VPD value.
-        """
-        # TODO: Make usable for at least the SCD30 or SCD40
-        air_T = dict["SCD30"]["Temperature"]
-        RH = dict["SCD30"]["Humidity"]
-        if (
-            not isinstance(air_T, float)
-            or not isinstance(RH, float)
-            or air_T <= 0.0
-            or RH <= 0.0
-        ):
-            raise Exception(
-                f"Received unexpected values for either the temperature ({air_T}) or humidity ({RH}) or both"
-            )
-        leaf_T = air_T - 2
-        vpd = 3.386 * (
-            math.exp(17.863 - 9621 / (leaf_T + 460))
-            - ((RH / 100) * math.exp(17.863 - 9621 / (air_T + 460)))
-        )
-        return vpd
 
     def _pid(self, setpoint: float, reading: float) -> int:
         """INTERNAL METHOD.  This is the code for the `PID controller <https://en.wikipedia.org/wiki/PID_controller>`_
