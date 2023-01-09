c3450e0e773be5bea55fcb965f95869b0cb3bc62
Happy Day <happyday.mjohnson@gmail.com>
2023-01-09 12:31:09 -0800

    evolved.

--- growbuddiesproject/growbuddies/snapbuddy_code.py
+++ growbuddiesproject/growbuddies/snapbuddy_code.py
@@ -19,7 +19,8 @@ class SnapBuddy:
         self.logger = LoggingHandler()
         if settings_string is None:
             self.logger.error(
-                "An empty string was provided as an input for a valid entry in the growbuddies_settings for a snapbuddy. Please provide a valid string representing an entry in the growbuddies_settings for the snapbuddy."
+                "An empty string was provided as an input for a valid entry in the growbuddies_settings for a snapbuddy. "
+                " Please provide a valid string representing an entry in the growbuddies_settings for the snapbuddy."
             )
             sys.exit()
         settings = Settings()
@@ -29,19 +30,20 @@ class SnapBuddy:
 
         if self.snapbuddy_settings_dict is None:
             self.logger.error(
-                f"The settings_string '{settings_string}' was not found in the growbuddies_settings. Please provide a valid string representing an entry in the growbuddies_settings for the snapbuddy."
+                f"The settings_string '{settings_string}' was not found in the growbuddies_settings. "
+                "Please provide a valid string representing an entry in the growbuddies_settings for the snapbuddy."
             )
             sys.exit()
         try:
             self.hostname = self.snapbuddy_settings_dict["hostname"]
-            self.interval_secs = self.snapbuddy_settings_dict["interval_secs"]
+            self._interval_secs = self.snapbuddy_settings_dict["interval_secs"]
             self.timelapse_dir = self.snapbuddy_settings_dict["timelapse_dir"]
         except KeyError as e:
             self.logger.error(
-                f"The settings_string '{settings_string}' was found in the growbuddies_settings but the key '{e}' was not found. Please provide a valid {e} : value pair in the growbuddies_settings for the snapbuddy"
+                f"The settings_string '{settings_string}' was found in the growbuddies_settings but the key '{e}' was not found. "
+                "Please provide a valid {e} : value pair in the growbuddies_settings for the snapbuddy"
             )
             sys.exit()
-
 
     def __repr__(self):
         return (
@@ -73,14 +75,26 @@ class SnapBuddy:
         # Check if the request was successful
         self.logger.debug(f"response code: {response.status_code}")
 
+    @property
+    def interval_secs(self):
+        return self._interval_secs
+
     def _get_filename(self):
         """Get the filename for the snapshot image.
         The filename is a combination of the timelapse directory and the current date and time.
         """
         # Make sure the timelapse directory exists
-        subdir = os.path.dirname(self.timelapse_dir)
-        if not os.path.exists(subdir):
-            os.makedirs(subdir)
+        # Split the file path into individual directories
+        dirs = self.timelapse_dir.split("/")
+        # remove the root directory
+        dirs.pop(0)
+
+        # Iterate through the directories, creating each one if it does not exist
+        subdir = ""
+        for i in range(len(dirs)):
+            subdir += "/" + dirs[i]
+            if not os.path.exists(subdir):
+                os.mkdir(subdir)
         filename = f"{self.timelapse_dir}/{datetime.datetime.now().strftime('%m%d%Y_%H%M%S')}.jpg"
         return filename
 
@@ -91,11 +105,12 @@ def main():
     snapbuddy = SnapBuddy("snapbuddy_settings_1")
     logger.debug(f"-> SnapBuddy class initialized. {snapbuddy}")
     while True:
-        time.sleep(10)
         try:
             snapbuddy.snap()
         except KeyboardInterrupt:
             sys.exit()
+
+        time.sleep(snapbuddy.interval_secs)
 
 
 if __name__ == "__main__":
