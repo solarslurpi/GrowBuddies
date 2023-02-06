db5fac2a60a5869d60b259b1002c38b51d0d588f
solarslurpi <solarslurpi@users.noreply.github.com>
2023-02-06 13:22:15 -0800

    Update helper_functions.py

--- growbuddiesproject/growbuddies/drgrowbuddy/helper_functions.py
+++ growbuddiesproject/growbuddies/drgrowbuddy/helper_functions.py
@@ -16,27 +16,35 @@ def check_zipfile(zip_file):
     Where class_name_x are the names of the image category classes and imagexxx.jpg are 
     the images in the dataset.  This function checks that the zip file is structured
     correctly and returns the names of the directories and the number of images in each 
-    directory.
-    """
-    with zipfile.ZipFile(zip_file) as archive:
-        names = archive.namelist()
-        directories = set()
+    directory.  
 
-        for name in names:
-            if not name.endswith(".jpg"):
-                raise ValueError(f"Error {name} does not end in .jpg. Was the zip file created with the Python script `transfer_files.py`")
-            components = name.split("/")[:-1]
-            if len(components) > 2:
-                raise ValueError(f"Directory structure is deeper than 2 levels: {name}")
-            for i in range(len(components)):
-                directories.add("/".join(components[:i + 1]))
-        jpg_count = {}
+    Example:  
+
+    .. code-block:: python
+
+        zip_file = "datasets.zip"
+        directories, jpg_count = check_zipfile(zip_file)
+        print(f"Done. \n-----\n{len(directories)} directories found.")
         for directory in directories:
-            jpg_count[directory] = len([name for name in names if name.startswith(directory) and name.endswith(".jpg")])
-        return sorted(list(directories)), jpg_count
+            print(f"  - {directory}: {jpg_count[directory]} .jpg files")
+    """
+    try:
+        with zipfile.ZipFile(zip_file) as archive:
+            names = archive.namelist()
+            directories = set()
 
-zip_file = "datasets.zip"
-directories, jpg_count = check_zipfile(zip_file)
-print(f"Done. \n-----\n{len(directories)} directories found.")
-for directory in directories:
-    print(f"  - {directory}: {jpg_count[directory]} .jpg files")
+            for name in names:
+                if not name.endswith(".jpg"):
+                    raise ValueError(f"Error {name} does not end in .jpg. Was the zip file created with the Python script `transfer_files.py`")
+                components = name.split("/")[:-1]
+                if len(components) > 2:
+                    raise ValueError(f"Directory structure is deeper than 2 levels: {name}")
+                for i in range(len(components)):
+                    directories.add("/".join(components[:i + 1]))
+            jpg_count = {}
+            for directory in directories:
+                jpg_count[directory] = len([name for name in names if name.startswith(directory) and name.endswith(".jpg")])
+            return sorted(list(directories)), jpg_count
+    except Exception as e:
+        print(f"Error: {e}")
+
