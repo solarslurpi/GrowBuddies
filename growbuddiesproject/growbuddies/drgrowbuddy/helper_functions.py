4b2be19afc6c4d94f6b07f1238f6b8acd1dda606
solarslurpi <solarslurpi@users.noreply.github.com>
2023-02-08 13:44:13 -0800

    Update helper_functions.py

--- growbuddiesproject/growbuddies/drgrowbuddy/helper_functions.py
+++ growbuddiesproject/growbuddies/drgrowbuddy/helper_functions.py
@@ -1,10 +1,26 @@
 from pathlib import Path
 from zipfile import ZipFile
+from PIL import Image
+import io
 import random
 
 def check_zipfile(zip_file):
-    with ZipFile(zip_file) as archive:
-        names = archive.namelist()
+    with ZipFile(zip_file) as zf:
+        names = zf.namelist()
+        # Verify that the contents of the zip file is an image.
+        # First select a random filename from the names list.
+        image_name = random.choice(names)
+        # Load the image into a buffer.
+        image_buffer = zf.read(image_name)
+        # Check if the image is a JPEG image.
+        try:
+            # io.BytesIO creates a binary stream from the contents of the buffer .
+            image = Image.open(io.BytesIO(image_buffer))
+            if image.format != 'JPEG':
+                raise ValueError('The image format is not JPEG.')
+            image.show(image_name)
+        except IOError:
+            raise ValueError('The contents of the buffer are not a valid image.')
         directories = set()
         for name in names:
             if not name.endswith(".jpg"):
@@ -18,14 +34,15 @@ def check_zipfile(zip_file):
         base_directory = "datasets"
         categories = [directory.split("/")[1] for directory in directories if directory != base_directory and directory.startswith(base_directory + "/")]
 
-        jpg_count = {category: len([name for name in names if category in name]) for category in categories}
-
+        jpg_count = {}
+        for category in categories:
+            jpg_count[category] = len([name for name in names if category in name])
         return jpg_count
 
-#zip_file = "drgb.zip"
-#jpg_count = check_zipfile(zip_file)
-#for key, value in jpg_count.items():
-#    print(f"Key: {key}, Value: {value}")
+# zip_file = "drgb.zip"
+# jpg_count = check_zipfile(zip_file)
+# for key, value in jpg_count.items():
+#     print(f"Key: {key}, Value: {value}")
 #------------------------------------------------------------------------------------------------------
 def pull_out_images(zip_filepath="drgb.zip", categories=["healthy","powdery_mildew"],nImages_per_directory=10):
 
